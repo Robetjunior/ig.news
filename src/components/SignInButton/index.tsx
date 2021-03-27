84d6f6d86bd7e6cceaf81c6a56ebb48614b1d433
Jose Roberto <juniorferreirarob@gmail.com>
2021-03-26 21:47:24 -0300

    first commit

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,26 @@
+import styles from './styles.module.scss'
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+
+export function SignInButton(){
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button 
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub color="#04d361"/>
+      Jose Roberto
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button 
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub color="#e3ab17"/>
+      Sign in with Github
+    </button>
+  )
+}
