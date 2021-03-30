ddde5b51d2015efacc0dee7d9c91e5fe49a90870
Jose Roberto <juniorferreirarob@gmail.com>
2021-03-30 20:52:08 -0300

    add Prismic CMS and use cloneElement

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,21 @@
+import {ActiveLink} from '../ActiveLink'
+
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
 export function Header() {
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news"/>
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Post</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch> 
+            <a>Post</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
